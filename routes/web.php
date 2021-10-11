4836e475954e02b3abca639943aa592988e82759
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-10-11 11:09:14 -0300

    adicionando botão fixo e agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,11 @@ use App\Http\Controllers\Admin\CidadeController;
 //     return view('welcome');
 // });
 
-Route::get('/', [CidadeController::class, 'cidades']);
+// redirect
+
+Route::redirect('/', '/admin/cidades');
+
+Route::prefix('admin')->group( function(){
+    Route::get('cidades', [CidadeController::class, 'cidades']);
+    Route::get('cidades/adicionar', [CidadeController::class, 'formAdicionar']);
+});
