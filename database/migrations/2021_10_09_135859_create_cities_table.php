be60e74923571ea2120039dca823ce5ba07b0c30
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-10-13 15:23:51 -0300

    Atribuição em massa

--- database/migrations/2021_10_09_135859_create_cities_table.php
+++ database/migrations/2021_10_09_135859_create_cities_table.php
@@ -17,6 +17,7 @@ class CreateCitiesTable extends Migration
             $table->id();
             $table->string('name', 100)->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
