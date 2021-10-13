428adf70423a876407830d5a35cb2e145b7c624c
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-10-13 16:57:05 -0300

    Exibindo feedback de sucesso ao inserir uma cidade

--- resources/views/admin/principal.blade.php
+++ resources/views/admin/principal.blade.php
@@ -35,13 +35,13 @@
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script>
+        @if (session('sucesso'))
+            M.toast({html: '{{ session('sucesso') }}'})
+        @endif
+    </script>
+
 </body>
 </html>
 
-
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  </head>
