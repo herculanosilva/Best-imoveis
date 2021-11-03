517538db5a0e09f2823d71755ce142a173b243c8
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-11-03 16:03:17 -0300

    rotas e excluindo registros

--- resources/views/admin/cidade/index.blade.php
+++ resources/views/admin/cidade/index.blade.php
@@ -13,7 +13,21 @@
                 @forelse ($cities as $city)
                     <tr>
                         <td>{{ $city->name }}</td>
-                        <td class="right-align">Excluir - Remover</td>
+                        <td class="right-align">
+                            <span>
+                                <i class="material-icons grey-text text-darken-2">edit</i>
+                            </span>
+                            <form action="{{ route('admin.cidades.deletar', $city->id) }}" method="post" style="display: inline">
+                                @csrf
+                                @method('DELETE')
+
+                                <button style="border:0;background:transparent;" type="submit">
+                                    <span style="cursor: pointer">
+                                        <i class="material-icons red-text text-accent-3">delete_forever</i>
+                                    </span>
+                                </button>
+                            </form>
+                        </td>
                     </tr>
                 @empty
                     <td colspan="2">Não existe cidade cadastrada!</td>
