bbd0d7007f3bc278d50843ab588892802a7ca77d
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-11-30 15:19:35 -0300

    reset project

--- resources/views/errors/401.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('errors.illustrated-layout')
-
-@section('title', "Acesso não autorizado")
-@section('code', '401')
-@section('message', "Desculpe, você não tem autorização para acessar está página.")
-@section('image')
-    <img src="{{ asset('img/Error-401.svg') }}">
-@endsection
--- resources/views/errors/429.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('errors.illustrated-layout')
-
-@section('title', "Muitas solicitações")
-@section('code', '429')
-@section('message', "Desculpe, você atingiu o limite de requesições, tente novamente mais tarde.")
-@section('image')
-    <img src="{{ asset('img/Error-429.svg') }}">
-@endsection
