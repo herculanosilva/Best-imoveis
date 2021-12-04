79a27a892109653c0a254490ab9e0dd697afbdd5
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-12-04 11:03:36 -0300

    Start Chart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,9 @@
 @section('conteudo-principal')
     <section class="section">
         <h2>implementar alguma coisas legal, tipo graficos e etc</h2>
+        {{-- charts --}}
+        {!! $chart->container() !!}
+        <script src="{{ $chart->cdn() }}"></script>
+        {{ $chart->script() }}
     </section>
 @endsection
