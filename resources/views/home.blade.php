cefffde3ee1e6f1c9472c84e432f62820317d24e
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-12-07 17:56:31 -0300

    class card-max

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,28 +3,28 @@
 @section('conteudo-principal')
         <section class="section section-daily-stats center">
             <div class="row">
-                <div class="col l3 m3 s3">
+                <div class="col m3 card-max">
                     <div class="card-panel red lighten-1 white-text center">
                         <i class="material-icons medium">location_on</i>
                         <h5>Cidades</h5>
                         <h3 class="count">{{ $qtd_cidades }}</h3>
                     </div>
                 </div>
-                <div class="col l3 m3 s3">
+                <div class="col m3 card-max">
                     <div class="card-panel red lighten-1 white-text center">
                         <i class="material-icons medium">location_city</i>
                         <h5>Tipos</h5>
                         <h3 class="count">{{ $qtd_tipos }}</h3>
                     </div>
                 </div>
-                <div class="col l3 m3 s3">
+                <div class="col m3 card-max">
                     <div class="card-panel red lighten-1 white-text center">
                         <i class="material-icons medium">gps_fixed</i>
                         <h5>Finalidades</h5>
                         <h3 class="count">{{ $qtd_finalidade }}</h3>
                     </div>
                 </div>
-                <div class="col l3 m3 s3">
+                <div class="col m3 card-max">
                     <div class="card-panel red lighten-1 white-text center">
                         <i class="material-icons medium">assignment_ind</i>
                         <h5>Usuários</h5>
@@ -33,7 +33,7 @@
                 </div>
           </section>
 
-        {{-- charts --}}
+          {{-- charts --}}
         {{-- {!! $chart->container() !!}
         <script src="{{ $chart->cdn() }}"></script>
         {{ $chart->script() }} --}}
