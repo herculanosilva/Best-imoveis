aa2202700f0b17245fee0948d9e4e3feb05ccce6
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-12-07 15:37:40 -0300

    Card panel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,84 +1,48 @@
 @extends('admin.layouts.principal')
 
 @section('conteudo-principal')
-    <section class="section">
-
-        <div class="row">
-            <div class="col s12 m4">
-                <div class="card red lighten-2">
-                    <div class="card-content white-text">
-                    <span class="card-title">Imóveis</span>
-                    <p class="center" style="font-size: 25px"><b>00</b></p>
-                    </div>
-                    <div class="card-action">
-                    <a href="#">Visualizar</a>
+        <section class="section section-daily-stats center">
+            <div class="row">
+                <div class="col l3 m3 s3">
+                    <div class="card-panel red lighten-1 white-text center">
+                        <i class="material-icons medium">location_on</i>
+                        <h5>Cidades</h5>
+                        <h3 class="count">{{ $qtd_cidades }}</h3>
                     </div>
                 </div>
-            </div>
-            <div class="col s12 m4">
-                <div class="card red lighten-2">
-                    <div class="card-content white-text">
-                    <span class="card-title">Cidades</span>
-                    <p class="center" style="font-size: 25px"><b>00</b></p>
-                    </div>
-                    <div class="card-action">
-                    <a href="#">Visualizar</a>
+                <div class="col l3 m3 s3">
+                    <div class="card-panel red lighten-1 white-text center">
+                        <i class="material-icons medium">location_city</i>
+                        <h5>Tipos</h5>
+                        <h3 class="count">{{ $qtd_tipos }}</h3>
                     </div>
                 </div>
-            </div>
-            <div class="col s12 m4">
-                <div class="card red lighten-2">
-                    <div class="card-content white-text">
-                    <span class="card-title">Tipos</span>
-                    <p class="center" style="font-size: 25px"><b>00</b></p>
-                    </div>
-                    <div class="card-action">
-                    <a href="#">Visualizar</a>
+                <div class="col l3 m3 s3">
+                    <div class="card-panel red lighten-1 white-text center">
+                        <i class="material-icons medium">gps_fixed</i>
+                        <h5>Finalidades</h5>
+                        <h3 class="count">{{ $qtd_finalidade }}</h3>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col s12 m4">
-                <div class="card red lighten-2">
-                    <div class="card-content white-text">
-                    <span class="card-title">Finalidades</span>
-                    <p class="center" style="font-size: 25px"><b>00</b></p>
-                    </div>
-                    <div class="card-action">
-                    <a href="#">Visualizar</a>
+                <div class="col l3 m3 s3">
+                    <div class="card-panel red lighten-1 white-text center">
+                        <i class="material-icons medium">assignment_ind</i>
+                        <h5>Usuários</h5>
+                        <h3 class="count">{{ $qtd_usuarios }}</h3>
                     </div>
                 </div>
-            </div>
-            <div class="col s12 m4">
-                <div class="card red lighten-2">
-                    <div class="card-content white-text">
-                    <span class="card-title">Usuários</span>
-                    <p class="center" style="font-size: 25px"><b>00</b></p>
-                    </div>
-                    <div class="card-action">
-                    <a href="#">Visualizar</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col s12 m4">
-                <div class="card red lighten-2">
-                    <div class="card-content white-text">
-                    <span class="card-title">??</span>
-                    <p class="center" style="font-size: 25px"><b>00</b></p>
-                    </div>
-                    <div class="card-action">
-                    <a href="#">Visualizar</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
+          </section>
 
         {{-- charts --}}
         {{-- {!! $chart->container() !!}
         <script src="{{ $chart->cdn() }}"></script>
         {{ $chart->script() }} --}}
-    </section>
+
 @endsection
+
+
+
+
+
+
+
