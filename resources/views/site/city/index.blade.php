9c7f9e2c9d3855b0bdb76e31c1f2ecb192d58d81
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-12-15 17:59:11 -0300

    footer

--- resources/views/site/city/index.blade.php
+++ resources/views/site/city/index.blade.php
@@ -46,3 +46,39 @@
         </ul>
     </section>
 @endsection
+
+@section('footer')
+<br>
+    <footer class="page-footer">
+        <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Best Imoveis</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+            </div>
+
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="#!">
+                            <i class="small material-icons">email</i>
+                        </a>
+                        <a class="grey-text text-lighten-3" href="#!">
+                            <i class="small material-icons">person_pin</i>
+                        </a>
+                        <a class="grey-text text-lighten-3" href="#!">
+                            <i class="small material-icons">code</i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </div>
+        <div class="footer-copyright">
+        <div class="container">
+        © 2021 Copyright Herculano Silva
+        </div>
+        </div>
+    </footer>
+@endsection
