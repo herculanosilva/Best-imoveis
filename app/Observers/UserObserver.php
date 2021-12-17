b8c103282f11fb9f70e87177ccec735fb60218b7
herculanosilva <herculano.silva@comparedistribuidora.com.br>
2021-12-17 17:25:30 -0300

    Start log user actions

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Log;
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        Log::create([
+            'action' => "Cadastro!",
+            'description' => "O usuário: $user->name foi cadastrado"
+        ]);
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        Log::create([
+            'action' => "Atualização",
+            'description' => "O usuário: $user->name foi atualizado"
+        ]);
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        Log::create([
+            'action' => "Remoção",
+            'description' => "O usuário: $user->name foi removido"
+        ]);
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
